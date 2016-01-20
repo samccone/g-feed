4dd6bc7211846b747a85a7ecf7f33d3f188d65c2
Sam Saccone <samccone@gmail.com>
2016-01-19 21:49:09 -0800

    Add sorting by time

--- components/app-feed/app-feed.js
+++ components/app-feed/app-feed.js
@@ -14,5 +14,15 @@
     setActiveTarget: function(e) {
       this.stateProvider.setActiveTarget(e.model.item.id);
     },
+
+    sortByTimestamp: function(a, b) {
+      a = moment(a).unix();
+      b = moment(b).unix();
+
+      if (a > b) return -1;
+      if (a < b) return 1;
+
+      return 0;
+    },
   });
 })();
